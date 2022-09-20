d8ac6c3603083cf52e3182d947ffc4b8bcb510bd
Aditya Naik <95295620+adityanik234@users.noreply.github.com>
2022-09-20 14:53:44 +0100

    Delete medbot directory

--- medbot/chatbot/test2.py
+++
@@ -1,5 +0,0 @@
-import csv
-import pandas as pd
-words = pd.read_table('/Users/adityanaik/Desktop/Summer project/CODE files/glove.840B.300d.txt', sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
-reza = words.iloc[0]
-print(reza)
